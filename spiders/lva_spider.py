442956ea233291dc579770cf58ff2b4fc67738ec
Hubercorp <hubertyj@gmail.com>
2021-02-09 21:40:59 +0100

    commit master

---
+++ spiders/lva_spider.py
@@ -0,0 +1,19 @@
+import scrapy
+
+
+class Lva_spider(scrapy.Spider):
+    name = "lva"
+
+    def start_requests(self):
+        urls = [
+            'https://www.lva-auto.fr/cote.php?src=menu',
+        ]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        page = response.url.split("/")[-2]
+        filename = f'quotes-{page}.html'
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+        self.log(f'Saved file {filename}')
